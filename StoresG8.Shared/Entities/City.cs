50f0c436ae0822b40335147e7bc5481c408bf4bd
Maria Alejandra Davila Gonzalez <alejandragonza.07@gmail.com>
2023-04-05 18:53:20 -0500

    Commit Nuevo

---
+++ StoresG8.Shared/Entities/City.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Stores.Shared.Entities
+{
+    public class City
+    {
+        // llave primary
+        public int Id { get; set; }
+
+        public int StateId { get; set; }
+
+        //No se le pone Icollection porque no tiene “hijos”
+        [Display(Name = "Ciudades/Municipios")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Name { get; set; } = null!;
+
+        public State? State { get; set; }
+    }
+}
+
