cbbb2ae70b1ee29ae230731b162a02269956510e
Maria Alejandra Davila Gonzalez <alejandragonza.07@gmail.com>
2023-04-19 18:01:36 -0500

    Commit

--- StoresG8.API/Services/IApiService.cs
+++ StoresG8.API/Services/IApiService.cs
@@ -1,8 +1,9 @@
-﻿
+﻿using StoresG8.WEB.Shared.Responses;
 namespace StoresG8.API.Services
 {
     using StoresG8.Shared.Responses;
-    using StoresG8.WEB.Shared.Responses;
+    
+
 
     // Creamos services cuando vamos a consumir una api
     namespace Stores.API.Services
